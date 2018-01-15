1fe18b7159f8291f074576077a10529beafa815b
knoll3 <noll.kyle@gmail.com>
2018-01-15 13:54:45 +0000

    DRY on test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,31 +11,14 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-  end
-
-  test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
-
-  end
+  pages = ["home", "help","about","contact"]
   
-  test "should get about" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | #{@base_title}"
-
+  pages.each do |page|
+    test "should get #{page}" do
+      eval("get static_pages_#{page}_url")
+      assert_response :success
+      assert_select "title", "#{page.capitalize} | #{@base_title}"
+    end
   end
-  
-  test "should get contact" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
-  end
-  
 
 end
