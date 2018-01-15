7e711316e444bcd2de08abf6bfee81331b6f45b2
knoll3 <noll.kyle@gmail.com>
2018-01-15 16:59:10 +0000

    Add method full_title to application helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,14 +11,28 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
-  pages = ["home", "help","about","contact"]
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
   
-  pages.each do |page|
-    test "should get #{page}" do
-      eval("get static_pages_#{page}_url")
-      assert_response :success
-      assert_select "title", "#{page.capitalize} | #{@base_title}"
-    end
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
