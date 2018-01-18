4893f910d5683cf654d1b685d028fdeb2adaec65
knoll3 <noll.kyle@gmail.com>
2018-01-18 14:13:35 +0000

    Add practice exercise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    
+    # Practice exercise
+    @t = Time.now
+    @hour = format("%02d", @t.hour)
+    @min =  format("%02d", @t.min )
+    @sec =  format("%02d", @t.sec )
   end
   
   def new
