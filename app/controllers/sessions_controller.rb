59e3c67f12f12c3b976f593d750c9b2d6d91eafb
knoll3 <noll.kyle@gmail.com>
2018-01-19 20:27:54 +0000

    Modify sessions controller and view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
+  
   def new
   end
+  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log the uder in and redirect tothe user's show page
+    else
+      # Create an error message
+      render 'new'
+    end
+  end
+  
 end
