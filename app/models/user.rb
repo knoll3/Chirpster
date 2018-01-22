ea8e9bdfb7af12c40977d8b2b89ab6c136eb7a47
knoll3 <noll.kyle@gmail.com>
2018-01-22 05:01:36 +0000

    Fix error
    End of 9.1.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
   end
   
   # Returns true if the given token matches the digest
-  def authenticated?
+  def authenticated?(remember_token)
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end
