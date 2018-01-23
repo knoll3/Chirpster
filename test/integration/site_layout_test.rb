e93902ea101c419e28824fb9f3eb58859ae6dc56
knoll3 <noll.kyle@gmail.com>
2018-01-23 22:58:39 +0000

    10.3.1 Users index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,11 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   
-  test "layout links" do
+  def setup
+    @user = users(:michael)
+  end
+  
+  test "home page layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
@@ -10,4 +14,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
+  
+  test "user page layout links while logged in" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select "div.container"
+    assert_select "a[href=?]", "/users/#{@user.id}"
+  end
+
 end
