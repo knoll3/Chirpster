789835e6e9717462ed5ab0b65dfbbcbea3b6b0aa
knoll3 <noll.kyle@gmail.com>
2018-01-25 19:58:55 +0000

    11.3.1 Generalizing the authenticated? method

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class UserMailerTest < ActionMailer::TestCase
   
   test "account_activation" do
-    user = users(:michael)
-    user.activation_token = User.new_token
-    mail = UserMailer.account_activation(user)
-    assert_equal "Account activation", mail.subject
-    assert_equal [user.email], mail.to
-    assert_equal ["noreply@example.com"], mail.from
-    assert_match user.name,               mail.body.encoded
-    assert_match user.activation_token,   mail.body.encoded
-    assert_match CGI.escape(user.email),  mail.body.encoded
+    user = users(:michael)                                       # Creates a new user loaded from users.yml
+    user.activation_token = User.new_token                       # Creates a 22 digit token
+    mail = UserMailer.account_activation(user)                   # Creates the message
+    assert_equal "Account activation",    mail.subject           # mail.subject comes from the account_activation method that was called
+    assert_equal [user.email],            mail.to                # mail.to also comes from the account_activation method that was called
+    assert_equal ["noreply@example.com"], mail.from              # mail.from defined in the application mailer
+    assert_match user.name,               mail.body.encoded      # Test will pass if mail.body.encoded contains the value for user.name
+    assert_match user.activation_token,   mail.body.encoded      # Test will pass if mail.body.encoded contains the value for user.activation_token
+    assert_match CGI.escape(user.email),  mail.body.encoded      # Test will pass if mail.body.encoded contains the value for CGI.escape(user.email)
   end
 end
