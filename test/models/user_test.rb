8cf31e59863799b6827d079b8d38fdae53bb7c54
knoll3 <noll.kyle@gmail.com>
2018-01-28 19:08:40 +0000

    13.1.4 Micropost refinements

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,14 +3,14 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-    @user = User.new name: "Example User", 
-                     email: "user@example.com",
-                     password: "foobar",
+    @user = User.new name:                  "Example User", 
+                     email:                 "user@example.com",
+                     password:              "foobar",
                      password_confirmation: "foobar"
   end
   
   test "should be valid" do
-    assert @user.valid?
+    assert @user.valid? 
   end
   
   test "name should be present" do
@@ -75,5 +75,13 @@ class UserTest < ActiveSupport::TestCase
   
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+  
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem_ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 end
